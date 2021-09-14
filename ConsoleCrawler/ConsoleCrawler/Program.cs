8c31eb2f7b81835f4f9462d3f80fd1028e8b5d15
Colby Cormier <nerdygamerz207@gmail.com>
2021-09-13 20:11:35 -0400

    First Commit

---
+++ ConsoleCrawler/ConsoleCrawler/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int stage = 1;
+
+            if (stage == 1)
+            {
+                Console.Write("/ ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.WriteLine("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.WriteLine("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.WriteLine("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.Write("A ");
+                Console.WriteLine("A ");
+            }
+
+        }
+    }
+}
