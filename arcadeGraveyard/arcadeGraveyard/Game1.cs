5233eb00435a263000602517d759a0ce6f1e37e3
Colby Cormier <nerdygamerz207@gmail.com>
2022-05-06 16:58:41 -0400

    Initial game commit

---
+++ arcadeGraveyard/arcadeGraveyard/Game1.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace arcadeGraveyard
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        // Player Assets
+        Texture2D tempPlayerTexture;
+        Player player;
+        Rectangle tempPlatform;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            tempPlayerTexture = Content.Load<Texture2D>("littleWhiteSquare");
+
+            player = new Player();
+            tempPlatform = new Rectangle(GraphicsDevice.Viewport.Width/2, GraphicsDevice.Viewport.Height/2 + 200, 64, 32);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (player.PlayerPos.X > tempPlatform.X)
+            {
+                player.IsGrounded = true;
+            }
+            else
+            {
+                player.IsGrounded = false;
+            }
+
+            player.Update();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(tempPlayerTexture, tempPlatform, Color.White);
+            _spriteBatch.Draw(tempPlayerTexture, player.PlayerPos, Color.White);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ arcadeGraveyard/arcadeGraveyard/Player.cs
@@ -0,0 +1,96 @@
+﻿
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace arcadeGraveyard
+{
+    class Player
+    {
+        // Player Variables
+        Rectangle playerRect;
+        KeyboardState kbState;
+        Vector2 velocity;
+        Vector2 position;
+        float gravityMultiplier = 1f;
+        bool isGrounded;
+
+        // Player Properties
+        //public Rectangle PlayerPos
+        //{
+        //    get { return playerRect; }
+        //}
+
+        public Vector2 PlayerPos
+        {
+            get { return position; }
+        }
+
+        public bool IsGrounded
+        {
+            get { return isGrounded; }
+            set { isGrounded = value; }
+        }
+
+        public Rectangle PlayerRect
+        {
+            get { return playerRect; }
+            set { playerRect = value; }
+        }
+
+        /// <summary>
+        /// Player constructor
+        /// </summary>
+        public Player()
+        {
+            playerRect = new Rectangle(0, 0, 32, 32);
+        }
+
+        /// <summary>
+        /// Player update method
+        /// </summary>
+        public void Update()
+        {
+            kbState = Keyboard.GetState();
+
+            velocity.X = 0;
+            velocity.Y = 0;
+
+            if (kbState.IsKeyDown(Keys.A))
+            {
+                velocity.X = -5f;
+            }
+
+            if (kbState.IsKeyDown(Keys.D))
+            {
+                velocity.X = 5f;
+            }
+
+            if (kbState.IsKeyDown(Keys.W) && isGrounded)
+            {
+                velocity.Y = -1f;
+            }
+
+            if (isGrounded)
+            {
+                gravityMultiplier = 1f;
+                //velocity.Y = 0;
+                isGrounded = true;
+            }
+            else
+            {
+                if (gravityMultiplier < 5)
+                {
+                    gravityMultiplier++;
+                }
+
+                velocity.Y = 0.15f * gravityMultiplier;
+                isGrounded = false;
+            }
+
+            //playerRect.X += (int)velocity.X;
+            //playerRect.Y += (int)velocity.Y;
+
+            position += velocity;
+        }
+    }
+}
